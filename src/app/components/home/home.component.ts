78dc47d417485de66aab413939811a57ccfaef3f
mejatingarg21 <mejatingarg21@gmail.com>
2025-08-19 20:11:50 +0530

    homepage

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,12 +1,4 @@
 import { Component, Inject, OnInit, PLATFORM_ID } from '@angular/core';
-import { isPlatformBrowser } from '@angular/common';
-
-interface Highlight {
-  type: 'hero' | 'highlight';
-  title: string;
-  description: string;
-  cta?: string;
-}
 
 @Component({
   selector: 'app-home',
@@ -14,30 +6,6 @@ interface Highlight {
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-
-  highlights: Highlight[] = [
-    {
-      type: 'hero',
-      title: 'Welcome to Rapid Pharmaceuticals',
-      description: 'Delivering trusted healthcare products across India since 1997.',
-      cta: 'Explore Our Products'
-    },
-    {
-      type: 'highlight',
-      title: 'Pan-India Delivery',
-      description: 'We supply to hospitals, pharmacies, and institutions across India.'
-    },
-    {
-      type: 'highlight',
-      title: 'Over 25 Years of Experience',
-      description: 'Serving the Indian healthcare system since 1997.'
-    },
-    {
-      type: 'highlight',
-      title: 'Trusted Brand',
-      description: 'Supplying medicines under our own reliable brand name.'
-    }
-  ];
 
   sampleProducts = [
     {
@@ -58,30 +26,7 @@ export class HomeComponent implements OnInit {
     },
   ];
 
-  currentSlide = 0;
-  intervalId: any;
-
-  constructor(@Inject(PLATFORM_ID) private platformId: Object) {}
-
   ngOnInit(): void {
-    if (isPlatformBrowser(this.platformId)) {
-      this.startCarousel();
-    }
   }
 
-  startCarousel(): void {
-    this.intervalId = setInterval(() => {
-      this.currentSlide = (this.currentSlide + 1) % this.highlights.length;
-    }, 5000);
-  }
-
-  goToSlide(index: number): void {
-    this.currentSlide = index;
-  }
-
-  ngOnDestroy(): void {
-    if (this.intervalId) {
-      clearInterval(this.intervalId);
-    }
-  }
 }
