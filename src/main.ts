b7055cce5d90e295e15c4ca334ea8d3bd596bcc8
mejatingarg21 <mejatingarg21@gmail.com>
2025-09-03 20:14:11 +0530

    requestpopup

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { bootstrapApplication } from '@angular/platform-browser';
+import { provideHttpClient, withFetch } from '@angular/common/http';
 
 import { AppModule } from './app/app.module';
+import { AppComponent } from './app/app.component';
 
 platformBrowserDynamic().bootstrapModule(AppModule, {
   ngZoneEventCoalescing: true
 })
   .catch(err => console.error(err));
+
+bootstrapApplication(AppComponent, {
+  providers: [
+    provideHttpClient(withFetch())
+  ]
+});
