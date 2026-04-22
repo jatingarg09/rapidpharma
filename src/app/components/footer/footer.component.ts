5f5c870370f0f0f7c58933545ef8b9741c9e875d
mejatingarg21 <mejatingarg21@gmail.com>
2026-04-23 00:06:58 +0530

    newfooter

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
     selector: 'app-footer',
     templateUrl: './footer.component.html',
     styleUrl: './footer.component.css',
-    standalone: true
+    standalone: true,
+    imports: [CommonModule, RouterModule, MatIconModule]
 })
 export class FooterComponent {
 
